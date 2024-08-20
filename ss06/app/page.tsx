0a755e2bbe29901f1332f0637127c7a59491cbdb
bao1405 <lqbbiker15052@gmail.com>
2024-08-20 08:05:41 +0700

    update

--- ss06/app/page.tsx
+++ ss06/app/page.tsx
@@ -1,6 +1,6 @@
-import Components from "@/components/page";
-import Pages from "@/pages/page";
-import Utils from "@/utils/page";
+import Components from "./components/page";
+import Pages from "./pages/page";
+import Utils from "./utils/page";
 import React from "react";
 
 export default function Home() {
